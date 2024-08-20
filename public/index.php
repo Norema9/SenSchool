d4b0cea97473f1c6279a99b8173636edb055fa7a
Norema9 <97368427+Norema9@users.noreply.github.com>
2024-08-20 20:01:08 +0200

    first_commit

--- public/index.php
+++ public/index.php
@@ -1,11 +1,40 @@
 <!DOCTYPE html>
-<html lang="en">
+<html>
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Academic Resource Manager</title>
+    <link rel="stylesheet" href="assets/css/index-style.css">
 </head>
 <body>
-    Hello world you wolllwfvsfvs
+    <div class="navbar">
+        <!-- Home -->
+        <a href="#home">Home</a>
+        
+        <!-- Secondaire -->
+        <div class="dropdown">
+            <button class="dropbtn">Secondaire</button>
+            <div class="dropdown-content">
+                <a href="jsp/layouts/viewResource.jsp?level=Secondaire&course=Sixieme">Sixième</a>
+                <a href="jsp/layouts/viewResource.jsp?level=Secondaire&course=Cinquieme">Cinquième</a>
+                <a href="jsp/layouts/viewResource.jsp?level=Secondaire&course=Quatrieme">Quatrième</a>
+                <a href="jsp/layouts/viewResource.jsp?level=Secondaire&course=Troisieme">Troisième</a>
+            </div>
+        </div>
+
+        <!-- Lycée -->
+        <div class="dropdown">
+            <button class="dropbtn">Lycée</button>
+            <div class="dropdown-content">
+                <a href="jsp/layouts/viewResource.jsp?level=Lycee&course=Seconde">Seconde</a>
+                <a href="jsp/layouts/viewResource.jsp?level=Lycee&course=Premiere">Première</a>
+                <a href="jsp/layouts/viewResource.jsp?level=Lycee&course=Terminale">Terminale</a>
+            </div>
+        </div>
+
+        <!-- Other menus as before -->
+    </div>
+
+    <div class="content">
+        Hello world
+    </div>
 </body>
 </html>
